d3d40a20c1356ed9e707ba8d2aafb8a4b7e5f30f
Rishabh Verma <rishitibest123@gmail.com>
2024-09-19 15:50:24 +0530

    Updated Changes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Stop any running instance of the app
+sudo pkill -f app.py
 
+# Navigate to the app folder
+cd /home/ubuntu/app
 
-# TODO your deploy script implementation...
+# Install required Python packages
+pip3 install -r requirements.txt
+
+# Start the Flask app
+nohup python3 app.py &
+
+echo "App deployed successfully!"
